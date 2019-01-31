e255e4ce4c629295c4fd4ea2bd6bcb127ff051ed
qc_0e17048632aa4b6e9dca5d0db48a36a8 <qc_0e17048632aa4b6e9dca5d0db48a36a8@git.code.tencent.com>
2019-01-31 10:03:45 +0800

    2019-1-31 edit pro.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ThemeComponent } from './theme/theme.component';
+import { LayoutModule } from './theme/layouts/layout.module';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ScriptLoaderService } from "./_services/script-loader.service";
+import { ThemeRoutingModule } from "./theme/theme-routing.module";
+import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    ThemeComponent,
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    LayoutModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ThemeRoutingModule,
+    AuthModule,
   ],
-  providers: [],
+  providers: [ScriptLoaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
