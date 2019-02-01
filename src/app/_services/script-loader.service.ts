f0edbf7ce915b1756963e788e5215153ce976589
qc_0e17048632aa4b6e9dca5d0db48a36a8 <qc_0e17048632aa4b6e9dca5d0db48a36a8@git.code.tencent.com>
2019-02-01 15:37:47 +0800

    2019-2-1 complete framework.

--- src/app/_services/script-loader.service.ts
+++ src/app/_services/script-loader.service.ts
@@ -1,49 +1,43 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router, NavigationStart, NavigationEnd } from '@angular/router';
-import { Helpers } from '../helpers';
-import { ScriptLoaderService } from '../_services/script-loader.service';
+import { Injectable } from "@angular/core";
+import * as $ from 'jquery';
 
-declare let mApp: any;
-declare let mUtil: any;
-declare let mLayout: any;
-@Component({
-  selector: ".m-grid.m-grid--hor.m-grid--root.m-page",
-  templateUrl: "./theme.component.html",
-  encapsulation: ViewEncapsulation.None,
-})
-export class ThemeComponent implements OnInit {
+declare let document: any;
 
+interface Script {
+  src: string;
+  loaded: boolean;
+}
 
-  constructor(private _script: ScriptLoaderService, private _router: Router) {
+@Injectable()
+export class ScriptLoaderService {
+  private _scripts: Script[] = [];
+  private tag: any;
 
+  load(tag, ...scripts: string[]) {
+    this.tag = tag;
+    scripts.forEach((script: string) => this._scripts[script] = { src: script, loaded: false });
+
+    let promises: any[] = [];
+    scripts.forEach((script) => promises.push(this.loadScript(script)));
+    return Promise.all(promises);
   }
-  ngOnInit() {
-    this._script.load('body', 'assets/vendors/base/vendors.bundle.js', 'assets/demo/default/base/scripts.bundle.js')
-      .then(result => {
-        Helpers.setLoading(false);
-        this._script.load('head', 'assets/vendors/custom/fullcalendar/fullcalendar.bundle.js')
-      });
-    this._router.events.subscribe((route) => {
-      if (route instanceof NavigationStart) {
-        (<any>mLayout).closeMobileAsideMenuOffcanvas();
-        (<any>mLayout).closeMobileHorMenuOffcanvas();
-        (<any>mApp).scrollTop();
-        Helpers.setLoading(true);
-        // hide visible popover
-        (<any>$('[data-toggle="m-popover"]')).popover('hide');
+
+  loadScript(src: string) {
+    return new Promise((resolve, reject) => {
+
+      //resolve if already loaded
+      if (this._scripts[src].loaded) {
+        resolve({ script: src, loaded: true, status: 'Already Loaded' });
       }
-      if (route instanceof NavigationEnd) {
-        // init required js
-        (<any>mApp).init();
-        (<any>mUtil).init();
-        Helpers.setLoading(false);
-        // content m-wrapper animation
-        let animation = 'm-animate-fade-in-up';
-        $('.m-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
-          $('.m-wrapper').removeClass(animation);
-        }).removeClass(animation).addClass(animation);
+      else {
+        //load script
+        let script = $('<script/>')
+          .attr('type', 'text/javascript')
+          .attr('src', this._scripts[src].src);
+
+        $(this.tag).append(script);
+        resolve({ script: src, loaded: true, status: 'Loaded' });
       }
     });
   }
-
 }
